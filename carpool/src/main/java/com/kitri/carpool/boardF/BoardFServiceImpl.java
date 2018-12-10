d871e7fef148d5a37dfff78f9b295dbdd22f8fb4
KITRI <KITRI@KITRI-PC>
2018-12-10 17:46:51 +0900

    service와 controller 업데이트

--- carpool/src/main/java/com/kitri/carpool/boardF/BoardFServiceImpl.java
+++ carpool/src/main/java/com/kitri/carpool/boardF/BoardFServiceImpl.java
@@ -2,47 +2,61 @@ package com.kitri.carpool.boardF;
 
 import java.util.ArrayList;
 
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+
+@Component("boardFService")
 public class BoardFServiceImpl implements BoardFService {
 
+	@Resource(name="sqlSession")
+	private SqlSession sqlSession;
 	private BoardFDao dao;
 
-	public BoardFServiceImpl(BoardFDao dao) {
-		this.dao = dao;
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
 	}
 	
 	@Override
 	public void add(BoardF b) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		dao.insert(b);
 	}
 
 	@Override
 	public void edit(BoardF b) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		dao.update(b);
 	}
 
 	@Override
 	public BoardF gettByNum(int num) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		return dao.selectByNum(num);
 	}
 
 	@Override
 	public ArrayList<BoardF> getByWriter(String writer) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		return dao.selectByWriter(writer);
 	}
 
 	@Override
 	public ArrayList<BoardF> getAll() {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		return dao.selectAll();
 	}
 
 	@Override
 	public void remove(int num) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardFDao.class);
 		dao.delete(num);
 	}
 
--- carpool/src/main/java/com/kitri/carpool/boardN/BoardNServiceImpl.java
+++ carpool/src/main/java/com/kitri/carpool/boardN/BoardNServiceImpl.java
@@ -2,47 +2,60 @@ package com.kitri.carpool.boardN;
 
 import java.util.ArrayList;
 
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+
+@Component("boardNService")
 public class BoardNServiceImpl implements BoardNService {
 
+	@Resource(name="sqlSession")
+	private SqlSession sqlSession;
 	private BoardNDao dao;
 
-	public BoardNServiceImpl(BoardNDao dao) {
-		this.dao = dao;
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
 	}
 	
 	@Override
 	public void add(BoardN b) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		dao.insert(b);
 	}
 
 	@Override
 	public void edit(BoardN b) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		dao.update(b);
 	}
 
 	@Override
 	public BoardN gettByNum(int num) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		return dao.selectByNum(num);
 	}
 
 	@Override
 	public ArrayList<BoardN> getByWriter(String writer) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		return dao.selectByWriter(writer);
 	}
 
 	@Override
 	public ArrayList<BoardN> getAll() {
-		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		return dao.selectAll();
 	}
 
 	@Override
 	public void remove(int num) {
 		// TODO Auto-generated method stub
+		dao = sqlSession.getMapper(BoardNDao.class);
 		dao.delete(num);
 	}
 
